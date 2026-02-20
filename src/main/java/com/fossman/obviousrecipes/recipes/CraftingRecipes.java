60d253d642533f7888edadaadb9d8fe1b8f6da4a
FOSSMan <137928236+0xfossman@users.noreply.github.com>
2026-02-20 11:36:12 +0100

    Upgrade Gradle wrapper to 8.12.1 for Loom compatibility

--- src/main/java/com/fossman/obviousrecipes/recipes/CraftingRecipes.java
+++
@@ -1,7 +0,0 @@
-package com.fossman.obviousrecipes.recipes;
-
-public class CraftingRecipes {
-    public void addCraftingRecipes() {
-        /* TODO add Crafting Recipes in Code instead of JSON Files */
-    }
-}
